e32191f9c9d031452a12079245a18408f381f4f9
Stauty91 <signmymousepad@googlemail.com>
2024-07-26 18:00:02 +0200

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 export const PORT = 5555;
 
 export const mongoDBURL = 
-'mongodb+srv://root:root@book-store-mern.ra6rqhm.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN';
+'mongodb+srv://root:root@book-store-mern.ra6rqhm.mongodb.net/cats-collection?retryWrites=true&w=majority&appName=Book-Store-MERN';
 
 // Please create a free database for Demo.
 // The Book-Store-Mern MongoDB will be deleted after project completion.
