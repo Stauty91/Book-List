a6090c43ee0e6a342e04a2566fb4abf99162903a
Stauty91 <signmymousepad@googlemail.com>
2024-07-26 15:38:26 +0200

    Commit

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
-import booksRoute from './routes/booksRoute.js';
+import booksRoute from './booksRoute.js';
 import cors from 'cors';
 
 const app = express();
