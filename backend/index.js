6e5f5b77f42f1b1e954e12d90604784b6488fc70
Stauty91 <signmymousepad@googlemail.com>
2024-07-26 16:59:20 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,9 +30,9 @@ mongoose
     .connect(mongoDBURL)
     .then(() => {
         console.log('App connected to database');
-        /*app.listen(PORT, () => {
+        app.listen(PORT, () => {
             console.log(`App is listening to port: ${PORT}`);
-        });*/
+        });
     })
     .catch((error) => {
         console.log(error);
